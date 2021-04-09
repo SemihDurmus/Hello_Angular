f3163eec49ec00115ceae82e0956bfd36a9fb1eb
SemihDurmus <semihdurmus@yahoo.com>
2021-04-09 23:05:11 +0200

    Add lifecycle hook OnInit

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Iproduct } from './product';
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -36,5 +36,8 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+  ngOnInit():void {
+    console.log('In OnInit')
   }
 }
