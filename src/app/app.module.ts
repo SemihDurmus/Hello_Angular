592562de591c9847451f3958cf8044df909a1d16
SemihDurmus <semihdurmus@yahoo.com>
2021-04-09 23:20:45 +0200

    Custom Pipe - covert to spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
