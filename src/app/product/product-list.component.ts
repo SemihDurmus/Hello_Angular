4a1e8526ca675bc61deba73bbbedd7f9c348fd78
SemihDurmus <semihdurmus@yahoo.com>
2021-04-10 15:03:15 +0200

    Add getter&setter filter products

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -3,14 +3,25 @@ import { Iproduct } from './product';
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter : ', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+  filteredProducts: Iproduct[] = [];
+
   products: Iproduct[] = [
     {
       productId: 1,
@@ -32,12 +43,48 @@ export class ProductListComponent implements OnInit{
       starRating: 4.2,
       imageUrl: 'assets/images/garden_cart.png',
     },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2021',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    },
+    {
+      productId: 8,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2021',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl: 'assets/images/saw.png',
+    },
+    {
+      productId: 10,
+      productName: 'Video Game Controller',
+      productCode: 'GMG-0042',
+      releaseDate: 'October 15, 2020',
+      description: 'Standard two-button video game controller',
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl: 'assets/images/xbox-controller.png',
+    },
   ];
 
+  performFilter(filterBy: string): Iproduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((item: Iproduct) =>
+      item.productName.toLocaleLowerCase().includes(filterBy)
+    );
+  }
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
-  ngOnInit():void {
-    console.log('In OnInit')
+  ngOnInit(): void {
+    this.listFilter = 'cart';
   }
 }
